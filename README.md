fc88fd8eb0cf89161303fcb1fb5481a2174bb625
Erysilva98 <erimilsonsilva98@gmail.com>
2021-10-19 21:47:11 -0300

    Versão Final

--- README.md
+++ README.md
@@ -1,23 +1,39 @@
 # Snake_game-JavaScript
+    Projeto Final da Digital Inovation One - HTML Web Developer
     Recriando o jogo da Cobrinha com JavaScript
 
 #### Iniciando a criação do Gamer
     - 19/10/2021
-    --------------------------------------------------
+    ---------------------------------------------------
     - Criando a Base dos arquivos html, javaScript
-    -  Aula 1 Concluida
+    -  Aula 1 Concluída
     ---------------------------------------------------
     - Criação de Movimentos 
-    - Aula 2 Concluida
+    - Aula 2 Concluída
     ---------------------------------------------------
     - Controle dos Movimentos
-    - Aula 3 Conluida 
+    - Aula 3 Conluída 
     ---------------------------------------------------
-    - Criando Icone da Comida
-    - Aula 4 Concluida
+    - Criando Ícone da Comida
+    - Aula 4 Concluída
     ---------------------------------------------------
-    -
-    - Aula 5
+    - Pausa ás 15h
+    - Retornado ás 19h50m
+    ---------------------------------------------------
+    - Ajuste da estrutura do gamer
+    - Gamer concluído
+    - Aula 5 Concluída
+    ---------------------------------------------------
+    - Estilizando a Página do nosso Gamer
+    - Aula 6 Concluída
+    - Finalizando aulas ás 20h25m
     - ************************************************ -
 
-#### 
+#### Adaptações aplicadas no pojeto
+    - Alterações das cores no Layout do game
+    - Ícone do titulo da página Adicionado
+    - Adicionado Link do Projeto no GitHub : Por Erimilson Silva
+    - Game Over! : Agora ao clicar em OK um novo jogo Começa.
+    - Entregando Projeto dia 19/10/2021
+    - Horário ás 22h
+    - ************************************************************ -
--- js/script.js
+++ js/script.js
@@ -16,18 +16,18 @@ let food = {
 
 
 function criarBG(){
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "lightblue";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function drawfood(){
-    context.fillStyle = "red";
+    context.fillStyle = "red";  /* Cor da Comida */    
     context.fillRect(food.x, food.y, box, box);
 }
 
 function criarCobrinha(){
     for(i = 0; i < snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = "blue";      /* Cor da Snake */
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
@@ -43,9 +43,17 @@ function update (event){
 function iniciarJogo(){
     if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
     if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
-
     if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
     if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
+    for( i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over! Click OK pra Reiniciar :)');
+            window.location.reload();
+
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -56,10 +64,16 @@ function iniciarJogo(){
 
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
-    if(direction == "up")   snakeX -= box;
+    if(direction == "up")   snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
